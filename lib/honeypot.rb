8d783bd178a98eac64ce907d893797d3bfa4a5a4
grosser <grosser.michael@gmail.com>
2008-11-07 09:21:01 +0100

    checkbox with value but unchecked, high tabindex to prevent users tabbing into it

--- lib/honeypot.rb
+++ lib/honeypot.rb
@@ -1,24 +1,25 @@
 module Honeypot
   module ControllerMethods
     def check_honeypots
-      return if honeypot_untouched?
-      flash[:notice] = 'You are a robot, or using some strange script.... no access for you..'
-      render :action => 'new'
+      return true if honeypot_untouched?
+      flash[:notice] = 'You are a robot, or using some strange script.... (if you are not, we overreacted, please contact us.)'
+      redirect_to :back
+      return false
     end
     
     def honeypot_untouched?
       submitted = params['its_so_sweet']
       return false if submitted.blank?
-      submitted['email'] == 'john@doe.com' && submitted['name'] == '' && submitted['agree']==nil
+      submitted['email'] == 'john@doe.com' && submitted['name'] == '' && submitted['agree'].blank?
     end
   end
   
   module ViewHelpers
     def honeypot
       content_tag('div', :style => 'position: absolute; left: -2000px;') do
-        text_field_tag('its_so_sweet[email]','john@doe.com') +
-        text_field_tag('its_so_sweet[name]','') + 
-        check_box_tag('its_so_sweet[agree]')
+        text_field_tag('its_so_sweet[email]','john@doe.com',:tabindex=>900) +
+        text_field_tag('its_so_sweet[name]','',:tabindex=>901) +
+        check_box_tag('its_so_sweet[agree]',1,false,:tabindex=>902)
       end
     end
   end
